2eda5a5b1b034228224f53583ff139d7e1750f29
Amrish Kushwaha <askmaurya48@gmail.com>
2019-07-13 19:50:27 +0530

    Add domain field

--- src/app/models/article.ts
+++ src/app/models/article.ts
@@ -7,4 +7,13 @@ export class Article {
     this.link = link;
     this.votes = votes || 0;
   }
+
+  domain(): string {
+    try {
+      const domainAndPath: string = this.link.split('//')[1];
+      return domainAndPath.split('/')[0]
+    } catch (err) {
+      return null;
+    }
+  }
 }
